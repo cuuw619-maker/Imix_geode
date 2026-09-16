0fb88cb947a78ac409c72f45bded78dbeeac51e2
cuuw619-maker <cuuw619@gmail.com>
2026-09-16 19:00:32 +0300

    Fix Imix popup API for Geode 5.10.1

--- src/ImixMenu.hpp
+++ src/ImixMenu.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
 #include <Geode/Geode.hpp>
+#include <Geode/ui/Popup.hpp>
 
-class ImixMenu : public geode::Popup<> {
+class ImixMenu : public geode::Popup {
 protected:
-    bool setup() override;
+    bool init();
 
 private:
-    void onCategory(CCObject* sender);
+    void onCategory(cocos2d::CCObject* sender);
     void selectCategory(int category);
     cocos2d::CCLayer* mContent = nullptr;
     cocos2d::CCArray* mCategoryButtons = nullptr;
     int mSelectedCategory = 0;
+
+public:
+    static ImixMenu* create();
 };
