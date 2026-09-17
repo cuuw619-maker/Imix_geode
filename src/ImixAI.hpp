a8a43c3d3c52ef031d92c19abc550de4d98e4c04
cuuw619-maker <cuuw619@gmail.com>
2026-09-17 13:06:15 +0300

    Prepare richer AI overlay and runtime state

--- src/ImixAI.hpp
+++ src/ImixAI.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-namespace cocos2d { class CCLabelTTF; }
+namespace cocos2d { class CCLayer; }
 class PlayLayer;
 
 namespace ImixAI {
     void update(PlayLayer* layer, float dt);
     void onDeath(PlayLayer* layer);
     bool enabled();
-    cocos2d::CCLabelTTF* createOverlay();
+    cocos2d::CCLayer* createOverlay();
     void reset();
 }
