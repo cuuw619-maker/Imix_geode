fdb048cb317cffabbef2a0d2934ea7aa48ff2491
cuuw619-maker <cuuw619@gmail.com>
2026-09-17 12:15:36 +0300

    Expose AI collision learning hook

--- src/ImixAI.hpp
+++ src/ImixAI.hpp
@@ -5,6 +5,7 @@ class PlayLayer;
 
 namespace ImixAI {
     void update(PlayLayer* layer, float dt);
+    void onDeath(PlayLayer* layer);
     bool enabled();
     cocos2d::CCLabelTTF* createOverlay();
     void reset();
