ed76df46f3fb7403c68469120250b62ce9ae3c2b
cuuw619-maker <cuuw619@gmail.com>
2026-09-17 10:54:04 +0300

    Fix Imix callback access for custom UI

--- src/ImixMenu.hpp
+++ src/ImixMenu.hpp
@@ -5,19 +5,24 @@
 class ImixMenu : public geode::Popup {
 protected:
     bool init();
-private:
+
+public:
+    static ImixMenu* create();
+
     void onCategory(cocos2d::CCObject* sender);
     void onSmartToggle(cocos2d::CCObject* sender);
     void onSmartAction(cocos2d::CCObject* sender);
     void onVisualToggle(cocos2d::CCObject* sender);
+    void onGameplayToggle(cocos2d::CCObject* sender);
     void onPlayerScale(cocos2d::CCObject* sender);
     void onResetFeatures(cocos2d::CCObject* sender);
+
+private:
     void selectCategory(int category);
     void animateContentIn();
     void animateCategoryButtons();
+
     cocos2d::CCLayer* mContent = nullptr;
     cocos2d::CCArray* mCategoryButtons = nullptr;
     int mSelectedCategory = 0;
-public:
-    static ImixMenu* create();
 };
